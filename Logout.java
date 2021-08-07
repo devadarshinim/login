a0d9c939a46695cc9354da19c4348471972d6e22
devadarshinim <65075996+devadarshinim@users.noreply.github.com>
2021-08-07 12:27:41 +0530

    Create Logout.java

---
+++ Logout.java
@@ -0,0 +1,21 @@
+package com.exam;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class Logout extends HttpServlet {
+    public void doGet(HttpServletRequest request, 
+    HttpServletResponse response) 
+      throws ServletException, IOException 
+  {  
+    HttpSession session = request.getSession(false);
+		session.removeAttribute("name");
+		session.invalidate();
+		response.sendRedirect("index.jsp");
+  }
+}
